6cd5f86521d3a7be60d054281888abf127f06ce4
forrestbenton <elliottbenton@gmail.com>
2018-07-28 18:47:43 -0400

    esperanto done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
-  # code here
+  e_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+
+  arr.sort_by do |string|
+    string.split("").collect do |i|
+      e_alpha.index(i)
+    end
+  end
+
 end
